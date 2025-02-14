95c94ea7f29a5ede16b7e2b8d12c09c2fa0a43af
Merwan Chaoui <merwan.chaoui.b@gmail.com>
2025-02-13 21:57:44 -0500

    clean and interpolate housing dataset

---
+++ datasets/Housing Dataset/interpolation script.py
@@ -0,0 +1,29 @@
+# the script relies on data from 1976 to 2011
+# first, it interpolates the value for each year
+# then, it forecasts the values for the years after (until 2026)
+
+import numpy as np
+import pandas as pd 
+
+df = pd.read_csv('testing.csv')
+df.set_index('year', inplace=True)
+
+# Extend index to 2026
+df = df.reindex(range(df.index.min(), 2027))
+
+# Interpolation for known years
+df.loc[:2011, 'high_growth'] = df.loc[:2011, 'high_growth'].interpolate(method='linear').round()
+
+# Forecast using linear regression
+known = df.loc[:2011, 'high_growth']
+coeffs = np.polyfit(list(known.index), known.values, 1)
+for year in df.index:
+    if year > 2011 and pd.isna(df.at[year, 'high_growth']):
+        df.at[year, 'high_growth'] = int(np.rint(np.polyval(coeffs, year)))
+
+# Reset index, rename, and export
+df.reset_index(inplace=True)
+df.rename(columns={'index': 'year', 'high_growth': 'growth'}, inplace=True)
+df['growth'] = df['growth'].astype(int)
+df.to_csv('growth_forecast.csv', index=False)
+print(df)
